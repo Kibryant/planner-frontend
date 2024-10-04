78900495a301f3a8844493d035f73102370abe0a
Kibryant <arthurgustavon@gmail.com>
2024-10-04 18:46:48 -0300

    hi

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,8 +2,7 @@ import Axios from "axios";
 
 export const api = Axios.create({
   baseURL:
-    process.env.EXPO_PUBLIC_BACKEND_URL ||
-    "https://c779-45-165-125-41.ngrok-free.app/api",
+    "https://planner-backend-zeta.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
