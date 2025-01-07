72db9b04111c0c12a7006eadef834f0816766d65
Kibryant <arthurgustavon@gmail.com>
2025-01-07 12:50:19 -0300

    fix url images

--- src/constants/corte-terapeutico.ts
+++ src/constants/corte-terapeutico.ts
@@ -6,51 +6,51 @@ export const corteTerapeutico = {
   links: [
     {
       name: "corte-terapeutico-1-feed.png",
-      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-teraupetico%2Fcorte-teraupetico-1-feed.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
+      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-terapeutico%2Fcorte-teraupetico-1-feed.png?alt=media&token=90bdce94-e534-4074-a61f-50ca7fe51422",
     },
     {
       name: "corte-terapeutico-1-story.png",
-      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-teraupetico%2Fcorte-teraupetico-1-story.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
+      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-terapeutico%2Fcorte-teraupetico-1-story.png?alt=media&token=d9084576-7650-4e9b-a721-1c5456afdf5f",
     },
     {
       name: "corte-terapeutico-2-feed.png",
-      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-teraupetico%2Fcorte-teraupetico-2-feed.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
+      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-terapeutico%2Fcorte-teraupetico-2-feed.png?alt=media&token=ce95a3f5-33d1-457d-aaa8-a1badfc784a7",
     },
     {
       name: "corte-terapeutico-2-story.png",
-      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-teraupetico%2Fcorte-teraupetico-2-story.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
+      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-terapeutico%2Fcorte-teraupetico-2-story.png?alt=media&token=296b06ab-521a-40ba-84ca-7052a0870eb1",
     },
     {
       name: "corte-terapeutico-3-feed.png",
-      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-teraupetico%2Fcorte-teraupetico-3-feed.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
+      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-terapeutico%2Fcorte-teraupetico-3-feed.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
     },
     {
       name: "corte-terapeutico-3-story.png",
-      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-teraupetico%2Fcorte-teraupetico-3-story.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
+      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-terapeutico%2Fcorte-teraupetico-3-story.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
     },
     {
       name: "corte-terapeutico-4-feed.png",
-      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-teraupetico%2Fcorte-teraupetico-4-feed.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
+      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-terapeutico%2Fcorte-teraupetico-4-feed.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
     },
     {
       name: "corte-terapeutico-4-story.png",
-      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-teraupetico%2Fcorte-teraupetico-4-story.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
+      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-terapeutico%2Fcorte-teraupetico-4-story.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
     },
     {
       name: "corte-terapeutico-5-feed.png",
-      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-teraupetico%2Fcorte-teraupetico-5-feed.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
+      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-terapeutico%2Fcorte-teraupetico-5-feed.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
     },
     {
       name: "corte-terapeutico-5-story.png",
-      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-teraupetico%2Fcorte-teraupetico-5-story.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
+      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-terapeutico%2Fcorte-teraupetico-5-story.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
     },
     {
       name: "corte-terapeutico-6-feed.png",
-      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-teraupetico%2Fcorte-teraupetico-6-feed.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
+      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-terapeutico%2Fcorte-teraupetico-6-feed.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
     },
     {
       name: "corte-terapeutico-6-story.png",
-      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-teraupetico%2Fcorte-teraupetico-6-story.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
+      url: "https://firebasestorage.googleapis.com/v0/b/planner-mecha-turbo.firebasestorage.app/o/corte-terapeutico%2Fcorte-teraupetico-6-story.png?alt=media&token=5595d1d1-98b2-49a1-a67d-2770e61ccea2",
     },
   ],
 };
